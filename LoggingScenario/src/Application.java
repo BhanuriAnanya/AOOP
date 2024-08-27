7d16bee7ac609b4120f5ac5ea856f602e84ac854
BhanuriAnanya <ananya.appy@gmail.com>
2024-08-27 10:46:52 +0530

    Add files via upload

---
+++ LoggingScenario/src/Application.java
@@ -0,0 +1,13 @@
+
+ class Application {
+	 public void performAction(String action) {
+	        
+	        Logger logger = Logger.getInstance();
+	        
+	        
+	        logger.log("Action performed: " + action);
+	    }
+	}
+
+
+
